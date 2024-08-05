6b7d888b19af1a7abe693a44bf25c22efb48db24
AiinuShil3a <Suppha1ak.sm@outlook.com>
2024-08-05 14:21:37 +0700

    id for Province

--- client/src/components/Card-Recomment-HomeStay.tsx
+++ client/src/components/Card-Recomment-HomeStay.tsx
@@ -83,7 +83,7 @@ const Card: React.FC<CardProps> = ({ item }) => {
           <div className="font-bold text-xl mb-2">
             {truncateText(item.name_homeStay || "", 15)}
           </div>
-          <p className="text-base">
+          <p id="Province-HomeStay" className="text-base">
             {truncateText(item.location[0].province_location || "", 30)}
           </p>
         </div>
