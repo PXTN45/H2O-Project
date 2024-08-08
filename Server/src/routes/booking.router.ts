f785dc616c873e89c95e027aa7a60e6684e3c793
sofekung0726 <sofekung@gmail.com>
2024-08-09 05:41:21 +0700

    fix router booking

--- Server/src/routes/booking.router.ts
+++ Server/src/routes/booking.router.ts
@@ -16,15 +16,15 @@ import verifyUser from "../middlewares/verifyUser";
 import verifyBusiness from "../middlewares/verifyBusiness";
 import verifyAdmin from "../middlewares/verifyAdmin";
 const router = express.Router();
-router.get("/booking",getBooking , verifyUser,verifyToken)
-router.get("/homestay-booking",getBookingHomeStayByUser , verifyUser,verifyToken)
-router.get("/package-booking",getBookingPackageByUser , verifyUser,verifyToken)
-router.post("/bookingHomeStay", bookHomeStay,verifyUser,verifyToken);
+router.get("/booking",getBooking , verifyAdmin,verifyToken)
+router.get("/homestay-booking/:userId",getBookingHomeStayByUser , verifyUser,verifyToken)
+router.get("/package-booking/:userId",getBookingPackageByUser , verifyUser,verifyToken)
+router.post("/bookingHomeStay/:userId", bookHomeStay,verifyUser,verifyToken);
 router.post("/bookingPackage", bookPackage,verifyUser,verifyToken);
-router.put("/editPackageBooking",editPackageBooking,verifyUser,verifyToken)
-router.put("/editHomeStayBooking",editHomeStayBooking,verifyUser,verifyToken)
+router.put("/editPackageBooking/:userId",editPackageBooking,verifyUser,verifyToken)
+router.put("/editHomeStayBooking/:userId",editHomeStayBooking,verifyUser,verifyToken)
 router.put("/confirmBooking/:id", confirmBooking,verifyBusiness,verifyToken);
-router.put("/cancelBooking",cancelBooking,verifyUser,verifyToken)
-router.delete("/deleteBooking",deleteBooking,verifyToken,verifyAdmin)
+router.put("/cancelBooking/:id",cancelBooking,verifyUser,verifyToken)
+router.delete("/deleteBooking/:id",deleteBooking,verifyToken,verifyAdmin)
 
 export default router;
